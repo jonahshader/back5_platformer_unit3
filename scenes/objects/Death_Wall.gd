80acc35bd084a9d65c3be3b373022fdd3dc44389
Ruar-p <rpodushkin@gmail.com>
2021-10-27 22:01:17 -0400

    death_wall
    Made a death_wall scene that makes the level reset when the player collides with it.

--- scenes/objects/Death_Wall.gd
+++ scenes/objects/Death_Wall.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
 export var speed = 300
-var vel = Vector2()
 
 func _process(delta):
 	position.x += delta * speed
 
-
 func _on_Death_Wall_body_entered(body):
-	pass # Replace with function body.
+	get_tree().reload_current_scene()
