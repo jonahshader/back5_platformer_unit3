8309e4bab59d0891ffb418c9798deb80247cb99e
Jonah Shader <jonah.shader@gmail.com>
2021-10-27 22:00:03 -0400

    added game over, win stuff to hud

--- scenes/menus/GameHud.gd
+++ scenes/menus/GameHud.gd
@@ -14,11 +14,34 @@ func calc_dist_left():
 	distance_left = player.position.distance_to(safehouse.position)
 	
 func _ready():
+	$ContinueButton.hide()
+	$RestartButton.hide()
+	$WinLabel.hide()
+	$LoseLabel.hide()
 	calc_dist_left()
 
 
 func _process(delta):
 	calc_dist_left()
-	$DistanceLeftLabel.text = "Distance Left: " + str(int(distance_left))
+	$DistanceLeftLabel.text = "Distance Left: " + str(int(distance_left / 50))
 	$TimeLabel.text = "Time: " + str(int(time))
 	time += delta
+	
+func gameover():
+	$LoseLabel.show()
+	yield(get_tree().create_timer(1.5), "timeout")
+	$RestartButton.show()
+
+func survived():
+	$WinLabel.show()
+	yield(get_tree().create_timer(1.5), "timeout")
+	$ContinueButton.show()
+	
+
+
+func _on_ContinueButton_pressed():
+	
+
+
+func _on_RestartButton_pressed():
+	pass # Replace with function body.
