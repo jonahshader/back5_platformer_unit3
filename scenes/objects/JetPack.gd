4465f188f95b0d622dfd862d3dd58822039f7337
Jonah Shader <jonah.shader@gmail.com>
2021-10-29 21:55:51 -0400

    got jetpack working

--- scenes/objects/JetPack.gd
+++ scenes/objects/JetPack.gd
@@ -1,45 +1,23 @@
 extends Node
 
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var on_player = false
 export var fuel_level = 5.0
 
 const THRUST_RATIO = 1.25
 onready var player = get_parent()
-onready var THRUST = get_node("../Player").gravity * THRUST_RATIO
+onready var THRUST = player.gravity * THRUST_RATIO
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var thrusting = false
-	if on_player:
-		if Input.is_action_pressed("jump"):
-			if fuel_level > 0:
-				player.vel.y -= THRUST * delta
-				fuel_level -= delta
-				thrusting = true
-				if fuel_level < 0:
-					fuel_level = 0
+	if Input.is_action_just_pressed("jump") and player.is_on_floor():
+		player.vel.y += player.jump_force/2 # counter act some of the jumping force
+	if Input.is_action_pressed("jump"):
+		if fuel_level > 0:
+			player.vel.y -= THRUST * delta
+			fuel_level -= delta
+			thrusting = true
+			if fuel_level < 0:
+				fuel_level = 0
+				queue_free()
 	$CPUParticles2D.emitting = thrusting
-
-
-func pickup():
-	if on_player == false:
-		#queue_free()
-		var new_jetpack = get_node(".")
-		new_jetpack.fuel_level = fuel_level
-		new_jetpack.on_player = true
-		on_player = true
-		get_node("../Player").add_child(new_jetpack)
-		
-		
-
-
-func _on_JetPack_body_entered(body):
-	pickup()
-	pass
